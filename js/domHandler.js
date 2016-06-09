9d4fff3fb605caf98ee6bf741406d8e01a084782
CJT1105 <CJT1105@GMAIL.COM>
2016-06-08 20:14:08 -0500

    added dom handler for french translation

--- js/domHandler.js
+++ js/domHandler.js
@@ -1,7 +1,25 @@
 /// this will change. it is just so you can see that the logic works
-
 var translate = document.querySelector('button');
+var translateValue = translate.value
+var french = document.querySelector('#french');
+var language = document.querySelector('.languagepicker');
+var transContainer = document.querySelector('#transContainer');
+
 translate.addEventListener('click', function(){
-	var frenchTranslation = Translator.translateToFrench();
-	console.log(frenchTranslation)
+	if (language.value === 'french'){
+		var frenchTranslation = Translator.translateToFrench();
+		frenchTranslation = frenchTranslation.join(' ');
+		transContainer.innerHTML = frenchTranslation;
+
+	}
 })
+
+
+
+
+
+
+// translate.addEventListener('click', function(){
+// 	var frenchTranslation = Translator.translateToFrench();
+// 	console.log(frenchTranslation)
+// })
