0140ea57b5ecdb0415a28f5f0e8241eaf00f93b2
Whitney Cormack <cormack.whitney@gmail.com>
2016-06-09 09:11:16 -0500

    spanish and domhandler round 1
    git status
    cler
    clear

---
+++ domHandlers.js
@@ -0,0 +1,9 @@
+
+var languagePicker = document.getElementById("languagePicker");
+
+languagePicker.addEventListener("change", function(translate) {
+	selectedLanguage = event.target.value;
+});
+
+
+
